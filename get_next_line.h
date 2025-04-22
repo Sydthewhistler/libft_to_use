e6bc27b6950e146a0827daf86478392e22633d09
Sydney Cavallin <scavalli@c3r1p4.42nice.fr>
2025-04-22 15:16:51 +0200

    ajout tout

---
+++ get_next_line.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: scavalli <scavalli@student.42nice.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/07 14:30:07 by scavalli          #+#    #+#             */
+/*   Updated: 2025/03/07 14:52:33 by scavalli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+# include <fcntl.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdint.h>
+# include <stdbool.h>
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 50
+# endif
+# define MAX_FD 1000
+
+char	*get_next_line(int fd);
+char	*ft_strjoin(char const *s1, char const *s2);
+size_t	ft_strlen(const char *str);
+void	*ft_calloc(size_t count, size_t size);
+bool	ft_strchr(const char *s, int c);
+
+#endif
